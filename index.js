38a72c174cea2443bffff3f16806ac4dd9ab423e
Tammy <tammyl@codevalue.net>
2019-08-01 15:50:25 +0300

    second-commit. Exercise incomplete

--- index.js
+++ index.js
@@ -14,18 +14,35 @@ function searchUsers(event) {
 }
 
 
-function getUserList() {
+async function getUserList() {
     const input = name.value;
 
-    fetch(`https://api.github.com/search/users?q=${input}`)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (response) {
-            console.log(usersList = JSON.stringify(response));
-            document.write(usersList);
+    let usersList = await fetch(`https://api.github.com/search/users?q=${input}`)
 
-        })
+    let user_details = usersList.items;
+
+    console.log(user_details);
+
+    let user_card = document.querySelector(".user-info-wrapper");
+
+    for (let i = 0; i < user_details.length; i++) {
+
+
+    }
+
+}
+
+function createUserDetailsCard(user_details) {
+    const card = `<
+    div class = "user-info-item1" >
+        <
+        img src = "https://avatars3.githubusercontent.com/u/39902?v=4>" / >
+        <
+        <button class="form-controls" id="user-repo" type="">User repositories </button>
+         < /div> <
+    div > Full Profile < /div> < /
+        div >`
+
 }
 
 
